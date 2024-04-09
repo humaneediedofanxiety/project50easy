b5a27dbb6c3b20fe0959bb475d9847272433f30e
Mahmudul Hasan <162016075+humaneediedofanxiety@users.noreply.github.com>
2024-04-10 03:38:06 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,80 @@
+// Time for the Functionality
+let taskItems = document.querySelectorAll("li");
+let count = document.getElementsByClassName('count')[0];
+
+let countIndex = localStorage.getItem('countIn')
+count.innerHTML = countIndex;
+if (countIndex === null) {
+    count.innerHTML = '0'
+}
+
+
+for (let i = 0; i < taskItems.length; i++) {
+    let task = taskItems[i].getAttribute('key')
+    console.log(task);
+    if (task === localStorage.getItem(`key${i}`)) {
+        // taskItems[i].setAttribute('id', `${i}`)
+        taskItems[i].classList.add('done');
+    }
+}
+
+for (let i = 0; i < taskItems.length; i++) {
+    taskItems[i].addEventListener('click', () => {
+        taskItems[i].classList.add('done')
+        // taskItems[i].setAttribute('id', `${i}`)
+        localStorage.setItem(`key${i}`,taskItems[i].getAttribute('key'))
+    })
+    taskItems[i].addEventListener('dblclick', (event) => {
+            taskItems[i].classList.remove('done');
+            localStorage.removeItem(`key${i}`)
+        
+    })
+}
+
+let currentDate = new Date();
+let currentHour = currentDate.getHours();
+let currentMin = currentDate.getMinutes();
+
+currentHour = currentHour < 10 ? "0" + currentHour : currentHour;
+currentMin = currentMin < 10 ? "0" + currentMin : currentMin;
+currentT = currentHour + currentMin;
+     
+function attrCheck() {
+    for (let i = 0; i < taskItems.length; i++){ 
+    if (taskItems[i].classList.contains('done') === false) {
+        return false;
+    }
+
+    }
+    return true;
+}
+
+let checkTrue = attrCheck();
+
+function runOnceDaily() {
+  // Get today's date in the format 'YYYY-MM-DD'
+  const today = new Date().toISOString().slice(0, 10);
+
+  // Check if the date key exists in localStorage
+    if (!localStorage.getItem('lastRunDate') || localStorage.getItem('lastRunDate') !== today) {
+      
+    if (checkTrue === true) {
+    localStorage.clear();
+    countIndex++
+    count.innerHTML = countIndex;
+    localStorage.setItem('countIn', countIndex);
+    console.log("gg you passed another day")
+    } else {
+        localStorage.clear()
+        alert('gg you failed :)))')
+    }
+
+    localStorage.setItem('lastRunDate', today);
+  } else {
+    console.log('Day Updated Once Already');
+  }
+}
+
+if (currentT > '0000') {
+    runOnceDaily();
+}
